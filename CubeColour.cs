e88b6cf6c5934c06d9af61955fbb26bffa9cbda9
Victor Elizalde <vic-bull@hotmail.com>
2017-10-02 16:26:13 -0500

    Set up

---
+++ CubeColour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColour : MonoBehaviour
+{
+    private Color originalColour;
+    private Vector3 originalVector;
+
+    // Use this for initialization
+    void Start()
+    {
+        var newColour = Color.blue; 
+        this.GetComponent<Renderer>().material.color = newColour;
+
+        originalColour = newColour;
+        originalVector = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnSelect()
+    {
+        // Get the object gazed at, then store original colour
+        var cube = this.GetComponent<Renderer>();
+
+        // Change to highlight colour
+        cube.material.color = Color.black;
+    }
+
+    void OnReset()
+    {
+        // Get the object gazed at, then store original colour
+        var cube = this.GetComponent<Renderer>();
+
+        // Change to highlight colour
+        cube.material.color = originalColour;
+    }
+
+    public void OnResetBlock()
+    {
+        this.transform.position = originalVector;
+    }
+}
---
+++ Shoot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class Shoot : MonoBehaviour
+{
+    GestureRecognizer recognizer;
+    public float ForceMagnitude = 300f;
+
+    private void ShootBall(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        var ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        // create a ball/spehere to shoot
+        ball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        // resize the ball
+        var rigidBody = ball.AddComponent<Rigidbody>();
+        // make tha ball adhere to gravity
+        rigidBody.mass = 0.5f;
+        // make the nall a little floaty, like a beer pong ball
+        rigidBody.position = transform.position;
+        // set the ball at the curret position
+        var transformForward = transform.forward;
+        // set the shoot angle
+        transformForward = Quaternion.AngleAxis(0, transform.right) * transformForward;
+        // shoot !!
+        rigidBody.AddForce(transformForward * ForceMagnitude);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += ShootBall;
+        recognizer.StartCapturingGestures();
+    }
+
+    public void OnShoot()
+    {
+        ShootBall(InteractionSourceKind.Voice, 1, new Ray());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
